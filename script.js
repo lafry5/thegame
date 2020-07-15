c8965f90bbdfad500cc9afd8ce426bec4fec6111
lafry5 <lafry5@yahoo.com>
2020-07-14 23:05:57 -0700

    added buttons on the power ups

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Counter
+var count = 0;
+var incrementEl = document.querySelector("#increment");
+var decrementEl = document.querySelector("#decrement");
+var done1El = document.querySelector("#done1");
+var done2El = document.querySelector("#done2");
+var done3El = document.querySelector("#done3");
+var done4El = document.querySelector("#done4");
+var done5El = document.querySelector("#done5");
+var done6El = document.querySelector("#done6");
+var done7El = document.querySelector("#done7");
+var done8El = document.querySelector("#done8");
+var done9El = document.querySelector("#done9");
+var done10El = document.querySelector("#done10");
+var done11El = document.querySelector("#done11");
+var done12El = document.querySelector("#done12");
+var countEl = document.querySelector("#count");
+
 var btn = document.querySelector('#search-button')
 var searchV = document.querySelector('#search-value')
 var name = document.querySelector('#name') 
@@ -21,7 +39,12 @@ for(i=0;i<myCityArray.length;i++){
    var parentList = document.querySelector(".list-group")
    var myLi = document.createElement("li")
    myLi.textContent = currentcity
-   myLi.addEventListener('click', function() {searchcities(this.textContent)})
+   myLi.addEventListener('click', function() {
+    //    count++;
+       console.log(count)
+    //    setCounterText()
+   })
+//    return count
 }
 parentList.append(myLi)
 console.log("Line 30 :- "+ currentcity);
@@ -29,31 +52,12 @@ console.log("mycityarray is" + myCityArray)
 return myCityArray
 }
 
+// setCounterText()
+
 btn.addEventListener('click', function() { 
     storecity(searchV.value)
-    buttonclick(searchV.value)
+    // buttonclick(searchV.value)
 }) //end of addEventListener button */
-
-
-// Counter
-var count = 0;
-var incrementEl = document.querySelector("#increment");
-var decrementEl = document.querySelector("#decrement");
-// var done1El = document.querySelector("#done1");
-// var done2El = document.querySelector("#done2");
-// var done3El = document.querySelector("#done3");
-// var done4El = document.querySelector("#done4");
-// var done5El = document.querySelector("#done5");
-// var done6El = document.querySelector("#done6");
-// var done7El = document.querySelector("#done7");
-// var done8El = document.querySelector("#done8");
-// var done9El = document.querySelector("#done9");
-// var done10El = document.querySelector("#done10");
-// var done11El = document.querySelector("#done11");
-// var done12El = document.querySelector("#done12");
-var countEl = document.querySelector("#count");
-
-
 
 
 incrementEl.addEventListener("click", function() {
@@ -70,5 +74,67 @@ decrementEl.addEventListener("click", function() {
 
 function setCounterText() {
     countEl.textContent = count;
-  }
+    // return count
+}
+
+done1El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done2El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done3El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done4El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done5El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done6El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done7El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done8El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done9El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done10El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done11El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
+done12El.addEventListener('click', function() {
+    count++;
+    setCounterText();
+});
+
 
